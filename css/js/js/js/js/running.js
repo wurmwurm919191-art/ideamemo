8793dda49e42af3fd5e4e8a1747b19aafbe6aa24
wurmwurm919191-art <wurmwurm919191@gmail.com>
2026-01-30 10:39:26 +0900

    Update running.js

--- css/js/js/js/js/running.js
+++ css/js/js/js/js/running.js
@@ -1,6 +1,9 @@
 const list = document.getElementById("runningList");
-getByStatus("running").forEach(m => {
-  const li = document.createElement("li");
-  li.textContent = m.text;
-  list.appendChild(li);
-});
+
+loadMemos()
+  .filter(m => m.status === "running")
+  .forEach(m => {
+    const li = document.createElement("li");
+    li.textContent = m.text;
+    list.appendChild(li);
+  });
