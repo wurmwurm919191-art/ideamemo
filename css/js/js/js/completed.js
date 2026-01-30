06d7e136584cc7374730c83802608abe9b1ccce7
wurmwurm919191-art <wurmwurm919191@gmail.com>
2026-01-30 11:03:08 +0900

    cleanup js folders

--- css/js/js/js/completed.js
+++
@@ -1,9 +0,0 @@
-const list = document.getElementById("completedList");
-
-loadMemos()
-  .filter(m => m.status === "completed")
-  .forEach(m => {
-    const li = document.createElement("li");
-    li.textContent = m.text;
-    list.appendChild(li);
-  });
--- css/js/js/js/js/running.js
+++
@@ -1,9 +0,0 @@
-const list = document.getElementById("runningList");
-
-loadMemos()
-  .filter(m => m.status === "running")
-  .forEach(m => {
-    const li = document.createElement("li");
-    li.textContent = m.text;
-    list.appendChild(li);
-  });
