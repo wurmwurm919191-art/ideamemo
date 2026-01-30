05c18f38391f63e744603cef223043ff93a958db
wurmwurm919191-art <wurmwurm919191@gmail.com>
2026-01-30 10:23:18 +0900

    Create completed.js

---
+++ css/js/js/js/completed.js
@@ -0,0 +1,8 @@
+const list = document.getElementById("completedList");
+loadMemos()
+  .filter(m => m.status === "completed")
+  .forEach(m => {
+    const li = document.createElement("li");
+    li.textContent = m.text;
+    list.appendChild(li);
+  });
