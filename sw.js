816b4b610a3fa903464a88f43194b6cd33f01c55
wurmwurm919191-art <wurmwurm919191@gmail.com>
2026-01-31 13:47:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
-self.addEventListener("install", () => {
+// 설치 시 바로 적용
+self.addEventListener("install", (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", () => {});
+// 활성화 시 모든 기존 캐시 삭제 (🔥 중요)
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => caches.delete(key)));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// 요청 시 캐시 확인 후 없으면 네트워크에서 받아오기
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
